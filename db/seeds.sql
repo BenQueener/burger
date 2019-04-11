4b071f08faea63190305a8ede2ff2242c4dc47a7
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-11 14:53:48 -0500

    Finished db files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name, devoured) VALUES ('Turkey', true);
+INSERT INTO burgers (name) VALUES ('BBQ');
+INSERT INTO burgers (name) VALUES ('Avocado');
+
