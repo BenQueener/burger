7ab599973eb18754b0af50e047538296f4ee1c8e
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-13 01:18:34 -0500

    heroku delpoyment updates

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
-  password: "root",
-  database: "burger_db"
+  password: "hacktheplanet",
+  database: "todoagain_db"
 });
 
 // Make connection.
